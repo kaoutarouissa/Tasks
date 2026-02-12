b1bf28e7cfd98cd3dbb328feb3c984195cf37ced
kaoutar <kaoutar@gmail.com>
2026-02-12 11:07:39 +0100

    fixer le widht du div ajouter

--- todo.js
+++ todo.js
@@ -2,15 +2,24 @@ export function todo(){
     const btn=document.getElementById("btnAjouter")
     const form=document.getElementById("forminput")
     const taskList=document.getElementById("taskList")
-btn.addEventListener('click', function(r){
-     r.preventDefault(); // empêcher le formulaire de reload page prc dert type submit f btn
-    const taskValeur=form.value
+    const msg=document.getElementById("emptyMessage")
+    const regex = /^[a-zA-Z0-9 ]+$/
 
+btn.addEventListener('click', function(r){
+msg.classList.add("hidden"); //ne pas afficher le msg vide
+    r.preventDefault(); // empêcher le formulaire de reload page prc dert type submit f btn
+    const taskValeur=form.value
+    if (!regex.test(taskValeur)) {
+  alert("La tâche doit contenir selement des lettres et ne pas commencer par espace ");
+  return;
+}
     const taskAjoute=document.createElement("div")
-    taskAjoute.className="bg-orange-300  p-4 rounded-2xl shadow-md flex justify-between items-center mb-2"
-// const taskValeur=form.value
-taskAjoute.textContent=taskValeur
-taskList.appendChild(taskAjoute)
+    taskAjoute.className="bg-pink-100 text-gray-700 min-w-0 m-6 text-lg font-semibold p-4 rounded-2xl shadow-md flex justify-between items-start mb-2w-full md:w-3/4 lg:w-1/2mx-auto break-words";
+    // const taskValeur=form.value
+    taskAjoute.textContent=taskValeur
+    taskList.appendChild(taskAjoute)
+    form.value="";
+
 })}
 
 
